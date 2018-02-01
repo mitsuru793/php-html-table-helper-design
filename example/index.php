b0e9ad6557786a79e9a1f716c568ac7bf7e0d11c
mitsuru793 <mitsuru793@gmail.com>
2018-02-02 02:14:43 +0900

    add example/index.php

---
+++ example/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Filesystem\Filesystem;
+
+$fs = new Filesystem();
+
+$files = scandir(__DIR__, 0);
+
+?>
+
+<ul>
+    <? foreach ($files as $file): ?>
+        <? if (preg_match("/^(\.\.?|index.php)/", $file)): ?>
+            <? continue ?>
+        <? endif ?>
+        <li>
+            <a href="<?= $file ?>"><?= $file ?></a>
+        </li>
+    <? endforeach ?>
+</ul>
